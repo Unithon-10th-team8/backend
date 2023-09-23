120aef2007a8dd9c6f5e7818ffa5fcbfe540d236
Daco2020 <dacokim32@gmail.com>
2023-09-23 16:59:59 +0900

    feat: 유저 CRUD 구현

--- app/views/user.py
+++ app/views/user.py
@@ -33,3 +33,65 @@ async def social_login(
     user = await user_service.get_or_create_user(provider, user_info["uid"])
     login(response, user)
     return user
+
+
+# TODO: 로그인 구현 후 활성화
+@router.get(
+    "/users/me",
+    status_code=HTTP_200_OK,
+    response_model=None,
+)
+async def get_me(
+    # current_user: schemas.UserProfile = Depends(deps.current_user),
+    user_service: UserService = Depends(deps.user_service),
+) -> None:
+    """내 정보를 조회합니다. (로그인 필요 후 활성화 예정)"""
+    return None
+    # user_profile = await user_service.get(current_user.id)
+    # return user_profile
+
+
+@router.get(
+    "/users",
+    status_code=HTTP_200_OK,
+    response_model=list[schemas.UserProfile],
+)
+async def fetch_users(
+    user_service: UserService = Depends(deps.user_service),
+    offset: int = 0,
+    limit: int = 100,
+) -> list[schemas.UserProfile]:
+    """복수 유저를 조회합니다."""
+    user_profiles = await user_service.fetch(offset=offset, limit=limit)
+    return user_profiles
+
+
+@router.post(
+    "/users/{user_id}",
+    status_code=HTTP_200_OK,
+    response_model=schemas.UserProfile,
+)
+async def update_user(
+    user_id: int,
+    # current_user: schemas.UserProfile = Depends(deps.current_user),
+    user_input: schemas.UserInput = Body(..., embed=True),
+    user_service: UserService = Depends(deps.user_service),
+) -> schemas.UserProfile:
+    """내 정보를 수정합니다."""
+    # TODO: 로그인 구현 후 유저 유효성 검사
+    await user_service.update(user_id, user_input)
+    user_profile = await user_service.get(user_id)
+    return user_profile
+
+
+@router.delete(
+    "/users/{user_id}",
+    status_code=HTTP_200_OK,
+)
+async def delete_user(
+    user_id: int,
+    # current_user: schemas.UserProfile = Depends(deps.current_user),
+    user_service: UserService = Depends(deps.user_service),
+) -> None:
+    """내 정보를 삭제합니다.(회원 탈퇴)"""
+    await user_service.delete(user_id)
