38703ccad4acf9c97d1cdecf554ee161ecc80aee
Daco2020 <dacokim32@gmail.com>
2023-09-24 00:32:05 +0900

    fix: 반환값 오류 수정

--- app/schemas.py
+++ app/schemas.py
@@ -9,8 +9,10 @@ class UserProfile(BaseModel):
     id: int = Field(description="유저 아이디", examples=[1])
     name: str = Field(description="이름", examples=["홍길동"])
     email: str = Field(description="이메일", examples=["user@email.com"])
-    profile_image_url: str = Field(
-        description="프로필 이미지 URL", examples=["https://www.google.com"]
+    profile_image_url: str | None = Field(
+        description="프로필 이미지 URL",
+        examples=["https://www.google.com"],
+        default=None,
     )
     created_at: datetime.datetime = Field(description="생성일시", examples=[tz_now()])
     updated_at: datetime.datetime = Field(description="변경일시", examples=[tz_now()])
